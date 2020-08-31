fb5b6588acf8e173c5d3a8709e805e39bb80f631
Yuqin Liang <yusnowsliang@gmail.com>
2020-08-31 14:45:57 +0800

    formating code

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 from export_mask import exportmask
 from writePly import writePly
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='integrate parameters')
     parser.add_argument("--dataset", type=str, help="the path of the dataset that you want to integrate")
@@ -28,7 +29,8 @@ def parse_args():
     parser.add_argument("--image_size", nargs='+', type=int, default=[512, 424], help="the soize of the images, [w, h]")
     args = parser.parse_args()
     return args
-    
+
+
 if __name__ == "__main__":
     args = parse_args()
     if args.dataset is None:
@@ -38,34 +40,31 @@ if __name__ == "__main__":
     if args.boundary is None:
         args.boundary = torch.LongTensor([0, 0, args.image_size[0], args.image_size[1]])
 
-    
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu" )
-    cameraparam = torch.tensor([[359,0,258],[0,360,209],[0,0,1]],dtype=torch.float32,device=device)
-    rgbdir = os.path.join(args.dataset,'images')
-    rgbfile = os.path.join(rgbdir, os.listdir(rgbdir)[0])
-    depthdir = os.path.join(args.dataset,'depth')
-    depthfile = os.path.join(depthdir,os.listdir(depthdir)[0])
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    cameraparam = torch.tensor([[359, 0, 258], [0, 360, 209], [0, 0, 1]], dtype=torch.float32, device=device)
+    rgbdir = os.path.join(args.dataset, 'images')
+    rgbfile = os.path.join(rgbdir, sorted(os.listdir(rgbdir))[0])
+    depthdir = os.path.join(args.dataset, 'depth')
+    depthfile = os.path.join(depthdir, sorted(os.listdir(depthdir))[0])
     rgb = Image.open(rgbfile)
     depth = Image.open(depthfile)
-    rgb = torch.tensor(np.array(rgb,dtype='float32'),device=device)
-    depth = torch.tensor(np.array(depth,dtype='float32'),device=device)
-    maskdir = os.path.join(args.dataset,'mask')
+    rgb = torch.tensor(np.array(rgb, dtype='float32'), device=device)
+    depth = torch.tensor(np.array(depth, dtype='float32'), device=device)
+    maskdir = os.path.join(args.dataset, 'mask')
     if not os.path.exists(maskdir):
         os.makedirs(maskdir)
     mask = exportmask(depth.cpu(), args.boundary, maskdir)
-    mask = torch.tensor(np.array(mask,dtype='float32'),device=device)
-    
+    mask = torch.tensor(np.array(mask, dtype='float32'), device=device)
+
     depthrefine = refine.DepthRefine(rgb, depth, mask, cameraparam)
-    refinedepth =  depthrefine.refine()
-    refinedir = os.path.join(args.dataset,'refined')
+    refinedepth = depthrefine.refine()
+    refinedir = os.path.join(args.dataset, 'refined')
     if not os.path.exists(refinedir):
         os.makedirs(refinedir)
     refinedimg = Image.fromarray(refinedepth)
-    refinedimgfile = os.path.join(refinedir,'refined.png')
+    refinedimgfile = os.path.join(refinedir, 'refined.png')
     refinedimg.save(refinedimgfile)
-    fragmentsdir = os.path.join(args.dataset,'fragments')
+    fragmentsdir = os.path.join(args.dataset, 'fragments')
     if not os.path.exists(fragmentsdir):
         os.makedirs(fragmentsdir)
     writePly(rgbfile, refinedimgfile, fragmentsdir)
-    
-    
--- writePly.py
+++ writePly.py
@@ -15,27 +15,45 @@ import os
 # outputfile = './data'
 def writePly(colorfile, depthfile, outputfile):
     volume = o3d.integration.ScalableTSDFVolume(
-    voxel_length = 4.0 / 512.0,
-    sdf_trunc = 0.04,
-    color_type = o3d.integration.TSDFVolumeColorType.RGB8)
-    
+        voxel_length=2.5 / 512.0,
+        sdf_trunc=0.04,
+        color_type=o3d.integration.TSDFVolumeColorType.RGB8)
+
     # for i in range(len(camera_poses)):
     #     print("Integrate {:d}-th image into the volume.".format(i))
     color = o3d.io.read_image(colorfile)
     depth = o3d.io.read_image(depthfile)
-    
+
     rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
-    color, depth, depth_scale = 10000 ,depth_trunc=2 , convert_rgb_to_intensity=False)
-    
+        color, depth, depth_scale=10000, depth_trunc=2, convert_rgb_to_intensity=False)
+
     volume.integrate(
-    rgbd,
-    o3d.camera.PinholeCameraIntrinsic(
-        o3d.camera.PinholeCameraIntrinsicParameters.Kinect2DepthCameraDefault),
-    np.identity(4))
-    
+        rgbd,
+        o3d.camera.PinholeCameraIntrinsic(
+            o3d.camera.PinholeCameraIntrinsicParameters.Kinect2DepthCameraDefault),
+        np.identity(4))
+
     mesh = volume.extract_triangle_mesh()
     mesh.compute_vertex_normals()
-    
-    output = os.path.join(outputfile,'fragment_000.ply')
-    
+
+    output = os.path.join(outputfile, 'fragment_000.ply')
+
     o3d.io.write_triangle_mesh(output, mesh)
+
+
+if __name__ == "__main__":
+    colorfile = "/home/yusnows/Projects/cv_lab/ScoModules/hardware/KinectCapSys/monocapture/images/images/regis_000000.jpg"
+    depthfile = "/home/yusnows/Projects/cv_lab/ScoModules/hardware/KinectCapSys/monocapture/images/depth/depth_000000.png"
+    from PIL import Image
+    depth = Image.open(depthfile)
+    import numpy as np
+    depth = np.array(depth)
+    dmask = np.ones((424, 512))*30000
+    dmask[58:221, 204:301] = depth[58:221, 204:301]
+    print(np.mean(dmask[58:221, 204:301]))
+    # dmask = dmask.astype(np.uint16)
+    # newdepth = Image.fromarray(dmask)
+    # newdepth.save("/home/yusnows/Projects/cv_lab/ScoModules/hardware/KinectCapSys/monocapture/images/depth/newdepth.png")
+    # depthfile = "/home/yusnows/Projects/cv_lab/ScoModules/hardware/KinectCapSys/monocapture/images/depth/newdepth.png"
+    # outputfile = "/home/yusnows/Projects/cv_lab/ScoModules/hardware/KinectCapSys/monocapture/images/"
+    # writePly(colorfile, depthfile, outputfile)
